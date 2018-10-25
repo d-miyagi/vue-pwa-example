08f5ebb344095bd886348b6628041450e58ac727
Daiki Miyagi <daiki@brcs.jp>
2018-10-25 18:23:41 +0900

    initial commit.

---
+++ public/serviceWorker.js
@@ -0,0 +1,63 @@
+// キャッシュデータ名
+var CACHE_STATIC_VERSION = 'static-v1';
+var CACHE_DYNAMIC_VERSION = 'dynamic-v1';
+
+// サービスワーカーのインストール
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker...');
+
+  // キャッシュできるまで次の処理を待つ
+  event.waitUntil(
+    caches.open(CACHE_STATIC_VERSION)
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App...');
+        cache.addAll([
+          '/',
+          './app.js'
+        ]);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetching something ...');
+  event.respondWith(
+    // キャッシュの存在チェック
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          // キャッシュがなければリクエストを投げて、レスポンスをキャッシュに入れる
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_VERSION)
+                .then(function(cache) {
+                  // 最後に res を返せるように、ここでは clone() する必要がある
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function() {
+              // TODO キャッシュ保存失敗時の処理
+            });
+        }
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker...');
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_VERSION && key !== CACHE_DYNAMIC_VERSION) {
+            console.log('[Service Worker] Removing old cache...');
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
+});
